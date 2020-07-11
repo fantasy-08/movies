324f132ab14397f7b336561b7f56f3ea0dd05065
fantasy-08 <59399447+fantasy-08@users.noreply.github.com>
2020-07-12 00:12:34 +0530

    firest react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React,{useState} from 'react';
+import Search from './Components/Search';
+import Results from './Components/Results';
+import Moreinfo from './Components/moreinfo';
+import axios from "axios";
 function App() {
+  const apiurl = "http://www.omdbapi.com/?apikey=dfe6d885";
+  const [state,setState] = useState({
+    s:"",//search query
+    results:[],
+    selected:{}
+  });
+  const search = (e)=>{
+    if(e.key==="Enter"){
+      axios(apiurl+"&s="+state.s).then(({data})=>{
+        console.log(data);
+        let resluts=data.Search;
+        console.log(data.Search);
+        setState(prevState=>{
+          return {...prevState,results:resluts}
+        })
+      });
+    }
+  }
+  const handleInput=(e)=>{
+    let s=e.target.value;
+    setState(prevState=>{
+      return {...prevState,s:s}
+    });
+    // console.log(state.s);
+  }
+  const openPopup=id=>{
+    axios(apiurl+"&i="+id).then(({data})=>{
+      let result=data;
+      // console.log(result);
+      setState(prevState=>{
+        return {...prevState,selected:result}
+      });
+    });
+  }
+  const closePop=()=>{
+    setState(prevState=>{
+      return {...prevState,selected:{}}
+    });
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Binge What?</h1>
       </header>
+      <main>
+        <Search handleInput={handleInput} search={search}/>
+        <Results results={state.results} openPopup={openPopup}/>
+        {(typeof state.selected.Title != "undefined")?<Moreinfo selected={state.selected} closePopup={closePop}/>:false}
+      </main>
     </div>
   );
 }
