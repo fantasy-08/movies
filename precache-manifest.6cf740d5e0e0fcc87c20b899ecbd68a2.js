bb0a1d81a0ed6a9415a084cf04bb289bd95f47c6
fantasy-08 <59399447+fantasy-08@users.noreply.github.com>
2020-07-12 00:28:47 +0530

    Updates

---
+++ precache-manifest.6cf740d5e0e0fcc87c20b899ecbd68a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af964544d13781c4ca1d2f34adaae4a6",
+    "url": "/movies/index.html"
+  },
+  {
+    "revision": "c918862af88fea0d7e9b",
+    "url": "/movies/static/css/main.f603110b.chunk.css"
+  },
+  {
+    "revision": "55695ad38d7bba590552",
+    "url": "/movies/static/js/2.64ea00fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movies/static/js/2.64ea00fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c918862af88fea0d7e9b",
+    "url": "/movies/static/js/main.7e1be7f0.chunk.js"
+  },
+  {
+    "revision": "45f1b12226fb8957f395",
+    "url": "/movies/static/js/runtime-main.6ee545b8.js"
+  }
+]);
